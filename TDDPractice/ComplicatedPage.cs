e5a61337f69ac292b623d03856061f2026f14aee
Jan Chalupa <jchalupa@outlook.com>
2018-06-29 15:54:00 +0200

    Browser drivers

---
+++ TDDPractice/ComplicatedPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace TDDPractice
+{
+    internal class ComplicatedPage
+    {
+        public ComplicatedPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        internal void Open()
+        {
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/complicated-page/");
+        }
+    }
+}
--- TDDPractice/UnitTest1.cs
+++ TDDPractice/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace TDDPractice
 {
@@ -10,6 +16,27 @@ namespace TDDPractice
         [TestMethod]
         public void TestMethod1()
         {
+            var driver = GetChromeDriver();
+            var complicatedPage = new ComplicatedPage(driver);
+            complicatedPage.Open();
+        }
+
+        private IWebDriver GetInternetExplorerDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new InternetExplorerDriver(outPutDirectory);
+        }
+
+        private IWebDriver GetFireFoxDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new FirefoxDriver(outPutDirectory);
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
         }
     }
 }
