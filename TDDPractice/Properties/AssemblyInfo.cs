cd4713029048f2a934bc83e93ddf6498ea3e0484
Jan Chalupa <jchalupa@outlook.com>
2018-06-29 15:15:47 +0200

    Add project files.

---
+++ TDDPractice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDPractice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("infor.com")]
+[assembly: AssemblyProduct("TDDPractice")]
+[assembly: AssemblyCopyright("Copyright © infor.com 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e6347ef-d278-4327-b7e2-6a7ed89576d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
